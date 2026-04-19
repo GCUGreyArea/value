e31d952d2d3b7c9ba11e2fd72ec2469c83d5c45b
Barry Robinson <barry.w.robinson64@gmail.com>
2026-04-19 10:29:28 +0100

    formating

--- lib/src/ParserDriver.cpp
+++ lib/src/ParserDriver.cpp
@@ -5,70 +5,80 @@
 #include <sstream>
 #include <stdexcept>
 
-using yyscan_t = void*;
+using yyscan_t = void *;
 
-int yyparse(ParserDriver& driver, yyscan_t scanner);
-int yylex_init_extra(ParserDriver* userDefined, yyscan_t* scanner);
+int yyparse(ParserDriver &driver, yyscan_t scanner);
+int yylex_init_extra(ParserDriver *userDefined, yyscan_t *scanner);
 int yylex_destroy(yyscan_t scanner);
-void yyset_in(FILE* inputFile, yyscan_t scanner);
+void yyset_in(FILE *inputFile, yyscan_t scanner);
 
-namespace {
+namespace
+{
 
-std::string trimCopy(const std::string& input) {
-    const size_t first = input.find_first_not_of(" \t\r\n");
-    if (first == std::string::npos) {
-        return "";
+    std::string trimCopy(const std::string &input)
+    {
+        const size_t first = input.find_first_not_of(" \t\r\n");
+        if (first == std::string::npos)
+        {
+            return "";
+        }
+
+        const size_t last = input.find_last_not_of(" \t\r\n");
+        return input.substr(first, last - first + 1);
     }
 
-    const size_t last = input.find_last_not_of(" \t\r\n");
-    return input.substr(first, last - first + 1);
-}
-
-size_t firstNonWhitespaceColumn(const ParsedField& field) {
-    const size_t offset = field.value.find_first_not_of(" \t\r\n");
-    return offset == std::string::npos ? field.column : field.column + offset;
-}
-
-std::string valueTypeToString(ValueType type) {
-    switch (type) {
-    case ValueType::INT:
-        return "INT";
-    case ValueType::LONG:
-        return "LONG";
-    case ValueType::STRING:
-        return "STRING";
-    case ValueType::UINT8:
-        return "UINT8";
-    case ValueType::UINT16:
-        return "UINT16";
-    case ValueType::UINT32:
-        return "UINT32";
-    case ValueType::UINT64:
-        return "UINT64";
-    case ValueType::BOOL:
-        return "BOOL";
-    case ValueType::DOUBLE:
-        return "DOUBLE";
-    case ValueType::FLOAT:
-        return "FLOAT";
+    size_t firstNonWhitespaceColumn(const ParsedField &field)
+    {
+        const size_t offset = field.value.find_first_not_of(" \t\r\n");
+        return offset == std::string::npos ? field.column : field.column + offset;
     }
 
-    return "UNKNOWN";
-}
+    std::string valueTypeToString(ValueType type)
+    {
+        switch (type)
+        {
+        case ValueType::INT:
+            return "INT";
+        case ValueType::LONG:
+            return "LONG";
+        case ValueType::STRING:
+            return "STRING";
+        case ValueType::UINT8:
+            return "UINT8";
+        case ValueType::UINT16:
+            return "UINT16";
+        case ValueType::UINT32:
+            return "UINT32";
+        case ValueType::UINT64:
+            return "UINT64";
+        case ValueType::BOOL:
+            return "BOOL";
+        case ValueType::DOUBLE:
+            return "DOUBLE";
+        case ValueType::FLOAT:
+            return "FLOAT";
+        }
 
-void rememberKeyOrder(std::vector<std::string>& keyOrder, const std::string& key) {
-    if (std::find(keyOrder.begin(), keyOrder.end(), key) == keyOrder.end()) {
-        keyOrder.push_back(key);
+        return "UNKNOWN";
     }
-}
+
+    void rememberKeyOrder(std::vector<std::string> &keyOrder, const std::string &key)
+    {
+        if (std::find(keyOrder.begin(), keyOrder.end(), key) == keyOrder.end())
+        {
+            keyOrder.push_back(key);
+        }
+    }
 
 } // namespace
 
-ParserDriver::ParserDriver(FlgParser& parserInstance) : parser(parserInstance) {
+ParserDriver::ParserDriver(FlgParser &parserInstance) : parser(parserInstance)
+{
     reset();
 }
 
-bool ParserDriver::parse(std::istream& input) {
+bool ParserDriver::parse(std::istream &input)
+{
     reset();
 
     std::ostringstream buffer;
@@ -77,7 +87,8 @@ bool ParserDriver::parse(std::istream& input) {
     sourceOffset = 0;
 
     yyscan_t scanner = nullptr;
-    if (yylex_init_extra(this, &scanner) != 0) {
+    if (yylex_init_extra(this, &scanner) != 0)
+    {
         addDiagnostic(DiagnosticSeverity::ERROR, 1, 1,
                       "Failed to initialise lexer");
         return false;
@@ -87,7 +98,8 @@ bool ParserDriver::parse(std::istream& input) {
     const int parseResult = yyparse(*this, scanner);
     yylex_destroy(scanner);
 
-    if (!headersParsed && parser.getErrors().empty()) {
+    if (!headersParsed && parser.getErrors().empty())
+    {
         addDiagnostic(DiagnosticSeverity::ERROR, lastLine == 0 ? 1 : lastLine, 1,
                       "Input did not contain a header row");
     }
@@ -95,35 +107,43 @@ bool ParserDriver::parse(std::istream& input) {
     return parseResult == 0 && parser.getErrors().empty();
 }
 
-void ParserDriver::consumeRow(const ParsedRow& row, size_t line, size_t endColumn) {
+void ParserDriver::consumeRow(const ParsedRow &row, size_t line, size_t endColumn)
+{
     lastLine = line;
 
-    if (!parser.getErrors().empty()) {
+    if (!parser.getErrors().empty())
+    {
         return;
     }
 
-    if (rowIsBlank(row)) {
+    if (rowIsBlank(row))
+    {
         return;
     }
 
-    if (parsingKeyValuePairs && !headersParsed) {
-        if (parser.expectedKVPairs > 0 && kvCount < parser.expectedKVPairs) {
+    if (parsingKeyValuePairs && !headersParsed)
+    {
+        if (parser.expectedKVPairs > 0 && kvCount < parser.expectedKVPairs)
+        {
             parseKeyValueRow(row, line);
             return;
         }
 
-        if (parser.expectedKVPairs > 0 && kvCount == parser.expectedKVPairs) {
+        if (parser.expectedKVPairs > 0 && kvCount == parser.expectedKVPairs)
+        {
             parsingKeyValuePairs = false;
             parseHeaderRow(row, line);
             return;
         }
 
-        if (row.size() == 2) {
+        if (row.size() == 2)
+        {
             parseKeyValueRow(row, line);
             return;
         }
 
-        if (row.size() > 2) {
+        if (row.size() > 2)
+        {
             parsingKeyValuePairs = false;
             parseHeaderRow(row, line);
             return;
@@ -140,21 +160,25 @@ void ParserDriver::consumeRow(const ParsedRow& row, size_t line, size_t endColum
 
 void ParserDriver::addSyntaxError(size_t line,
                                   size_t column,
-                                  const std::string& message) {
+                                  const std::string &message)
+{
     lastLine = line;
     addDiagnostic(DiagnosticSeverity::ERROR, line, column, message);
 }
 
-char ParserDriver::getDelimiter() const {
+char ParserDriver::getDelimiter() const
+{
     return parser.delimiter;
 }
 
-int ParserDriver::readInput(char* buffer, int maxSize) {
+int ParserDriver::readInput(char *buffer, int maxSize)
+{
     const size_t remaining = sourceContent.size() - sourceOffset;
     const size_t count =
         std::min(static_cast<size_t>(maxSize), remaining);
 
-    if (count == 0) {
+    if (count == 0)
+    {
         return 0;
     }
 
@@ -163,76 +187,91 @@ int ParserDriver::readInput(char* buffer, int maxSize) {
     return static_cast<int>(count);
 }
 
-size_t ParserDriver::lexerLine() const {
+size_t ParserDriver::lexerLine() const
+{
     return lexerState.line;
 }
 
-size_t ParserDriver::lexerColumn() const {
+size_t ParserDriver::lexerColumn() const
+{
     return lexerState.column;
 }
 
-bool ParserDriver::lexerExpectField() const {
+bool ParserDriver::lexerExpectField() const
+{
     return lexerState.expectField;
 }
 
-bool ParserDriver::lexerLineHasContent() const {
+bool ParserDriver::lexerLineHasContent() const
+{
     return lexerState.lineHasContent;
 }
 
-void ParserDriver::lexerConsumeDelimiter() {
+void ParserDriver::lexerConsumeDelimiter()
+{
     lexerState.lineHasContent = true;
     lexerState.expectField = true;
     ++lexerState.column;
 }
 
-void ParserDriver::lexerConsumeNewline() {
+void ParserDriver::lexerConsumeNewline()
+{
     ++lexerState.line;
     lexerState.column = 1;
     lexerState.expectField = true;
     lexerState.lineHasContent = false;
 }
 
-void ParserDriver::lexerConsumeField(size_t length) {
+void ParserDriver::lexerConsumeField(size_t length)
+{
     lexerState.lineHasContent = true;
     lexerState.expectField = false;
     lexerState.column += length;
 }
 
-void ParserDriver::lexerEmitSyntheticField() {
+void ParserDriver::lexerEmitSyntheticField()
+{
     lexerState.expectField = false;
 }
 
-bool ParserDriver::lexerHasPendingBoundary() const {
+bool ParserDriver::lexerHasPendingBoundary() const
+{
     return lexerState.pendingBoundary != BoundaryToken::NONE;
 }
 
-ParserDriver::BoundaryToken ParserDriver::lexerPendingBoundary() const {
+ParserDriver::BoundaryToken ParserDriver::lexerPendingBoundary() const
+{
     return lexerState.pendingBoundary;
 }
 
-size_t ParserDriver::lexerPendingBoundaryLine() const {
+size_t ParserDriver::lexerPendingBoundaryLine() const
+{
     return lexerState.pendingBoundaryLine;
 }
 
-size_t ParserDriver::lexerPendingBoundaryColumn() const {
+size_t ParserDriver::lexerPendingBoundaryColumn() const
+{
     return lexerState.pendingBoundaryColumn;
 }
 
 void ParserDriver::lexerStorePendingBoundary(BoundaryToken token,
                                              size_t line,
-                                             size_t column) {
+                                             size_t column)
+{
     lexerState.pendingBoundary = token;
     lexerState.pendingBoundaryLine = line;
     lexerState.pendingBoundaryColumn = column;
 }
 
-void ParserDriver::lexerClearPendingBoundary() {
+void ParserDriver::lexerClearPendingBoundary()
+{
     lexerState.pendingBoundary = BoundaryToken::NONE;
     lexerState.pendingBoundaryLine = 0;
     lexerState.pendingBoundaryColumn = 0;
 }
 
-void ParserDriver::reset() {
+void ParserDriver::reset()
+{
     parser.kvPairs.clear();
     parser.columns.clear();
     parser.dataRows.clear();
@@ -258,12 +297,15 @@ void ParserDriver::reset() {
 void ParserDriver::addDiagnostic(DiagnosticSeverity severity,
                                  size_t line,
                                  size_t column,
-                                 const std::string& message) {
+                                 const std::string &message)
+{
     parser.diagnostics.push_back({line, column, severity, message});
 }
 
-void ParserDriver::parseKeyValueRow(const ParsedRow& row, size_t line) {
-    if (row.size() < 2) {
+void ParserDriver::parseKeyValueRow(const ParsedRow &row, size_t line)
+{
+    if (row.size() < 2)
+    {
         addDiagnostic(DiagnosticSeverity::ERROR, line,
                       firstNonWhitespaceColumn(row.front()),
                       "Expected a key-value pair separated by '" +
@@ -271,7 +313,8 @@ void ParserDriver::parseKeyValueRow(const ParsedRow& row, size_t line) {
         return;
     }
 
-    if (row.size() > 2) {
+    if (row.size() > 2)
+    {
         addDiagnostic(DiagnosticSeverity::ERROR, line, row[2].column - 1,
                       "Too many delimiters in key-value pair");
         return;
@@ -279,25 +322,32 @@ void ParserDriver::parseKeyValueRow(const ParsedRow& row, size_t line) {
 
     const std::string key = trimCopy(row[0].value);
     const std::string value = trimCopy(row[1].value);
-    if (key.empty()) {
+    if (key.empty())
+    {
         addDiagnostic(DiagnosticSeverity::ERROR, line, 1,
                       "Key-value pair is missing a key");
         return;
     }
 
-    if (!validateFieldValue(key, row[1], line)) {
+    if (!validateFieldValue(key, row[1], line))
+    {
         return;
     }
 
     Value parsedValue;
     if (value.find('.') != std::string::npos &&
         tryParseValue(value, ValueType::DOUBLE, parsedValue) &&
-        std::holds_alternative<double>(parsedValue)) {
+        std::holds_alternative<double>(parsedValue))
+    {
         parser.kvPairs.set(key, std::get<double>(parsedValue));
-    } else if (tryParseValue(value, ValueType::LONG, parsedValue) &&
-               std::holds_alternative<long>(parsedValue)) {
+    }
+    else if (tryParseValue(value, ValueType::LONG, parsedValue) &&
+             std::holds_alternative<long>(parsedValue))
+    {
         parser.kvPairs.set(key, std::get<long>(parsedValue));
-    } else {
+    }
+    else
+    {
         parser.kvPairs.set(key, value);
     }
 
@@ -305,20 +355,26 @@ void ParserDriver::parseKeyValueRow(const ParsedRow& row, size_t line) {
     ++kvCount;
 }
 
-void ParserDriver::parseHeaderRow(const ParsedRow& row, size_t line) {
+void ParserDriver::parseHeaderRow(const ParsedRow &row, size_t line)
+{
     headersParsed = true;
 
-    for (size_t columnIndex = 0; columnIndex < row.size(); ++columnIndex) {
+    for (size_t columnIndex = 0; columnIndex < row.size(); ++columnIndex)
+    {
         ColumnDefinition column;
         column.name = trimCopy(row[columnIndex].value);
         column.type = ValueType::STRING;
 
         const auto indexIt = parser.columnIndexTypeMap.find(columnIndex);
-        if (indexIt != parser.columnIndexTypeMap.end()) {
+        if (indexIt != parser.columnIndexTypeMap.end())
+        {
             column.type = indexIt->second;
-        } else {
+        }
+        else
+        {
             const auto nameIt = parser.columnTypeMap.find(column.name);
-            if (nameIt != parser.columnTypeMap.end()) {
+            if (nameIt != parser.columnTypeMap.end())
+            {
                 column.type = nameIt->second;
             }
         }
@@ -329,19 +385,22 @@ void ParserDriver::parseHeaderRow(const ParsedRow& row, size_t line) {
     validateRequiredHeadings(line);
 }
 
-void ParserDriver::parseDataRow(const ParsedRow& row,
+void ParserDriver::parseDataRow(const ParsedRow &row,
                                 size_t line,
-                                size_t endColumn) {
+                                size_t endColumn)
+{
     ValueVector parsedRow;
 
     for (size_t columnIndex = 0;
          columnIndex < row.size() && columnIndex < parser.columns.size();
-         ++columnIndex) {
+         ++columnIndex)
+    {
         Value parsedValue;
         const std::string trimmedField = trimCopy(row[columnIndex].value);
         const ValueType expectedType = parser.columns[columnIndex].type;
 
-        if (!tryParseValue(trimmedField, expectedType, parsedValue)) {
+        if (!tryParseValue(trimmedField, expectedType, parsedValue))
+        {
             addDiagnostic(DiagnosticSeverity::ERROR, line, row[columnIndex].column,
                           "Unexpected type for column '" +
                               parser.columns[columnIndex].name + "': expected " +
@@ -352,15 +411,18 @@ void ParserDriver::parseDataRow(const ParsedRow& row,
 
         if (!validateFieldValue(parser.columns[columnIndex].name,
                                 row[columnIndex],
-                                line)) {
+                                line))
+        {
             return;
         }
 
         parsedRow.push(parsedValue);
     }
 
-    if (row.size() != parser.columns.size()) {
-        if (row.size() > parser.columns.size()) {
+    if (row.size() != parser.columns.size())
+    {
+        if (row.size() > parser.columns.size())
+        {
             addDiagnostic(DiagnosticSeverity::ERROR, line,
                           row[parser.columns.size()].column,
                           "Too many delimiters in data row");
@@ -375,20 +437,25 @@ void ParserDriver::parseDataRow(const ParsedRow& row,
     parser.dataRows.push_back(parsedRow);
 }
 
-bool ParserDriver::validateFieldValue(const std::string& fieldName,
-                                      const ParsedField& field,
-                                      size_t line) {
+bool ParserDriver::validateFieldValue(const std::string &fieldName,
+                                      const ParsedField &field,
+                                      size_t line)
+{
     const auto validatorIt = parser.fieldValidatorMap.find(fieldName);
-    if (validatorIt == parser.fieldValidatorMap.end()) {
+    if (validatorIt == parser.fieldValidatorMap.end())
+    {
         return true;
     }
 
     std::string errorMessage;
     const std::string trimmedValue = trimCopy(field.value);
     StringFieldValidator validator;
-    try {
+    try
+    {
         validator = getValidator(validatorIt->second);
-    } catch (const std::exception& exception) {
+    }
+    catch (const std::exception &exception)
+    {
         addDiagnostic(DiagnosticSeverity::ERROR,
                       line,
                       firstNonWhitespaceColumn(field),
@@ -397,7 +464,8 @@ bool ParserDriver::validateFieldValue(const std::string& fieldName,
         return false;
     }
 
-    if (validator(trimmedValue, errorMessage)) {
+    if (validator(trimmedValue, errorMessage))
+    {
         return true;
     }
 
@@ -411,19 +479,24 @@ bool ParserDriver::validateFieldValue(const std::string& fieldName,
     return false;
 }
 
-bool ParserDriver::rowIsBlank(const ParsedRow& row) const {
+bool ParserDriver::rowIsBlank(const ParsedRow &row) const
+{
     return row.size() == 1 && trimCopy(row.front().value).empty();
 }
 
-void ParserDriver::validateRequiredHeadings(size_t line) {
-    for (const auto& requirement : parser.requiredHeadings) {
+void ParserDriver::validateRequiredHeadings(size_t line)
+{
+    for (const auto &requirement : parser.requiredHeadings)
+    {
         const auto it = std::find_if(
             parser.columns.begin(), parser.columns.end(),
-            [&requirement](const ColumnDefinition& column) {
+            [&requirement](const ColumnDefinition &column)
+            {
                 return column.name == requirement.first;
             });
 
-        if (it == parser.columns.end()) {
+        if (it == parser.columns.end())
+        {
             addDiagnostic(requirement.second, line, 1,
                           "Required heading missing: " + requirement.first);
         }
